8dc56645b2bfb2503eac825313e653d6f72b7abc
Rachel Freeland <rachel.freeland2@gmail.com>
2021-05-09 21:40:35 -0500

    Updated the code block for the multiplyArray function and the README to reflect the change

--- starterCode/app.js
+++ starterCode/app.js
@@ -72,8 +72,8 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-  let firstSum = sum(sumArr[0], sumArr[1]);
-  let secondSum = sum(firstSum[0], sumArr[2]);
+
+  let secondSum = sumAndMultiply(sumArr[0], sumArr[1], sumArr[2]); 
 
   return [secondSum[0], sumArr + ' was passed in as an array of numbers, and ' + secondSum[0] + ' is their sum.']
 }
@@ -94,10 +94,12 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
 
+  let multiplyTotal = sumAndMultiply(multArr[0], multArr[1], multArr[2]);
+  return [multiplyTotal[1], 'The numbers ' + multArr + ' have a product of ' + multiplyTotal[1] + '.'];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-//testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
